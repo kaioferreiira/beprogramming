6455f2be1a1fb99eade3ec8ae68365b1f3ae80b2
kaiocosta <kaio.costa@zallpy.com>
2022-02-08 15:01:46 -0300

    first commit

---
+++ src/main/java/br/com/beprogramming/BeProgrammingApplication.java
@@ -0,0 +1,13 @@
+package br.com.beprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/beprogramming/BeProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.beprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
